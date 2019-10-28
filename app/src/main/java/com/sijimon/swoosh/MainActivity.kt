1d82e032a083db15d659e412dacf253537c0fb07
rakheesc <chakkithara@gmail.com>
2019-10-27 20:34:44 -0400

    Initial commit

---
+++ app/src/main/java/com/sijimon/swoosh/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sijimon.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
